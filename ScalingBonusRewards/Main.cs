8e5a938be2d9c6301609bf3a0bafc91431290c42
Keira Dueck <sylae@calref.net>
2025-06-01 05:24:25 -0700

    wip, just seeing if harmony is done correctly

--- ScalingBonusRewards/Main.cs
+++ ScalingBonusRewards/Main.cs
@@ -1,17 +1,31 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
 using UnityModManagerNet;
+using DV.Logic.Job;
+using DV.ThingTypes;
+using dnlib;
+using UnityEngine;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using dnlib.DotNet.Pdb;
 
-namespace MOD_NAME;
+namespace ScalingBonusRewards;
 
+[EnableReloading]
 public static class Main
 {
-	// Unity Mod Manage Wiki: https://wiki.nexusmods.com/index.php/Category:Unity_Mod_Manager
+	public static Settings? settings;
+
 	private static bool Load(UnityModManager.ModEntry modEntry)
 	{
-		Harmony? harmony = null;
+		settings = Settings.Load<Settings>(modEntry);
+		modEntry.OnGUI = OnGUI;
+		modEntry.OnSaveGUI = OnSaveGUI;
 
+
+		Harmony? harmony = null;
 		try
 		{
 			harmony = new Harmony(modEntry.Info.Id);
@@ -27,5 +41,60 @@ public static class Main
 		}
 
 		return true;
+	}
+
+	static void OnGUI(UnityModManager.ModEntry modEntry)
+	{
+		settings.Draw(modEntry);
+	}
+
+	static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+	{
+		settings?.Save(modEntry);
+	}
+
+	static bool Unload(UnityModManager.ModEntry modEntry)
+	{
+		Harmony harmony = new Harmony(modEntry.Info.Id);
+		harmony.UnpatchAll();
+
+		return true;
+	}
+}
+
+public class Settings : UnityModManager.ModSettings, IDrawable
+{
+	[Draw("Maximum Possible Bonus Multiplier")] public float maxBonusCoef = 1.0f;
+
+	public override void Save(UnityModManager.ModEntry modEntry)
+	{
+		Save(this, modEntry);
+	}
+
+	public void OnChange()
+	{
+	}
+}
+
+[HarmonyPatch(typeof(Job), nameof(Job.GetBonusPaymentForTheJob))]
+static class Patch_GetBonusPaymentForTheJob
+{
+	static void Postfix(ref float __result, Job __instance)
+	{
+		if (__instance.State == JobState.Completed)
+		{
+			if (__instance.GetJobCompletionTime() <= __instance.TimeLimit + 60f)
+			{
+				__result = __instance.GetPotentialBonusPaymentForTheJob();
+				return;
+			}
+		}
+		else if (__instance.State == JobState.InProgress && __instance.GetTimeOnJob() <= __instance.TimeLimit + 60f)
+		{
+			__result = __instance.GetPotentialBonusPaymentForTheJob();
+			return;
+		}
+		__result = 0f;
+		return;
 	}
 }
